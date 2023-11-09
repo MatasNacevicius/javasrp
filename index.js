21b45884caabcbc393ab944ea12e25e2c9c58b19
MatasNacevicius <matas.nacevicius@stud.kitm.lt>
2023-11-09 11:31:14 +0200

    new commit

---
+++ index.js
@@ -0,0 +1,39 @@
+// let abc = "labas";
+// abc = "ate"
+// console.log(abc)
+
+// let a = 1;
+// console.log(a++)
+// console.log(a)
+// console.log(++a)
+
+// let b = 5;
+// console.log(--b);
+// console.log(--b);
+// console.log(--b);
+
+// let c = "5";
+// console.log(typeof Number(c));
+// console.log(typeof parseInt(c, 10));
+// console.log(typeof parseFloat(c));
+
+// let sk1 = 5;
+// let sk2 = 8 - sk1;
+// console.log(sk2);
+
+// let petro_ugis = 189;
+// let petro_svoris = 95;
+// let jono_ugis = 174;
+// let jono_svoris = 104;
+
+// const petro_KMI = petro_svoris / ((petro_ugis / 100) ** 2)
+// const jono_KMI = jono_svoris / ((jono_ugis / 100) ** 2)
+
+// console.log(`Petro KMI: ${petro_KMI.toFixed(2)}. Jono KMI: ${jono_KMI.toFixed(2)}. Jono KMI yra didesnis uz Petro`);
+
+
+// let a1 = 10;
+// let b1 = 12;
+
+// const c1 =Math.sqrt((a1**2) + (b1**2));
+// console.log(c1.toFixed(2))
